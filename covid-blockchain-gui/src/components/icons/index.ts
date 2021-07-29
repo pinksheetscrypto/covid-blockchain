6708b506146eb609979ae60734d8430e4ec62fc2
randywessels <rwessels@cox.net>
2021-07-28 20:51:40 -0700

    Fix case problem preventing GUI from working in Unix

--- covid-blockchain-gui/src/components/icons/index.ts
+++ covid-blockchain-gui/src/components/icons/index.ts
@@ -1,4 +1,4 @@
-export { default as Covid } from './Covid';
+export { default as Covid } from './covid';
 export { default as Farm } from './Farm';
 export { default as Home } from './Home';
 export { default as Keys } from './Keys';
