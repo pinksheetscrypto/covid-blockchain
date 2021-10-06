443f8ecf3358b9a0be12a01971cb593d8664d76f
Randy Wessels <xamlz@outlook.com>
2021-10-06 03:18:37 -0700

    rename

--- covid/full_node/mempool_check_conditions.py
+++ covid/full_node/mempool_check_conditions.py
@@ -3,19 +3,19 @@ import time
 from typing import Dict, List, Optional
 from clvm_rs import STRICT_MODE
 
-from chia.consensus.cost_calculator import NPCResult
-from chia.full_node.generator import create_generator_args, setup_generator_args
-from chia.types.blockchain_format.program import NIL
-from chia.types.coin_record import CoinRecord
-from chia.types.condition_with_args import ConditionWithArgs
-from chia.types.generator_types import BlockGenerator
-from chia.types.name_puzzle_condition import NPC
-from chia.util.clvm import int_from_bytes
-from chia.util.condition_tools import ConditionOpcode
-from chia.util.errors import Err
-from chia.util.ints import uint32, uint64, uint16
-from chia.wallet.puzzles.generator_loader import GENERATOR_FOR_SINGLE_COIN_MOD
-from chia.wallet.puzzles.rom_bootstrap_generator import get_generator
+from covid.consensus.cost_calculator import NPCResult
+from covid.full_node.generator import create_generator_args, setup_generator_args
+from covid.types.blockchain_format.program import NIL
+from covid.types.coin_record import CoinRecord
+from covid.types.condition_with_args import ConditionWithArgs
+from covid.types.generator_types import BlockGenerator
+from covid.types.name_puzzle_condition import NPC
+from covid.util.clvm import int_from_bytes
+from covid.util.condition_tools import ConditionOpcode
+from covid.util.errors import Err
+from covid.util.ints import uint32, uint64, uint16
+from covid.wallet.puzzles.generator_loader import GENERATOR_FOR_SINGLE_COIN_MOD
+from covid.wallet.puzzles.rom_bootstrap_generator import get_generator
 
 GENERATOR_MOD = get_generator()
 
