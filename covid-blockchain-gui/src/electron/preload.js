7b9cc1bc1e872bd6b1b75e150a9252aabab1e174
Randy Wessels <xamlz@outlook.com>
2021-10-06 21:04:02 -0700

    preload

--- covid-blockchain-gui/src/electron/preload.js
+++ covid-blockchain-gui/src/electron/preload.js
@@ -1,8 +1,7 @@
-const { ipcRenderer,shell } = require('electron');
+const electron = require('electron');
 
 const geoip =require("geoip-lite");
 
-window.remote = electron.remote;
 window.ipcRenderer = electron.ipcRenderer;
 window.shell = electron.shell;
 window.geoip = geoip;
